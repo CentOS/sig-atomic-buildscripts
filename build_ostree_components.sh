44665116400349578630aedba5499f30d65fda45
Karanbir Singh <kbsingh@karan.org>
2015-05-19 21:24:18 +0100

    turn on epel for now, we will need to resolve this before the final builds

--- build_ostree_components.sh
+++ build_ostree_components.sh
@@ -37,7 +37,7 @@ set -o pipefail
 
 # Init, make sure we have the bits we need installed. 
 cp -f rhel-atomic-rebuild.repo /etc/yum.repos.d/
-yum -y install ostree rpm-ostree docker libvirt
+yum -y install ostree rpm-ostree docker libvirt epel-release
 
 cp -f atomic7-testing.repo /etc/yum.repos.d/
 echo 'enabled=0' >> /etc/yum.repos.d/atomic7-testing.repo
