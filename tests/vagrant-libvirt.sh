bf2591e4e95c447a4cd2e19b5c2843aecef719a6
Jason Brooks <jbrooks@redhat.com>
2016-05-19 10:21:54 -0700

    add sclo-vagrant1-vagrant-libvirt (#79)

--- tests/vagrant-libvirt.sh
+++ tests/vagrant-libvirt.sh
@@ -3,7 +3,7 @@
 # this script assumes its being run on CentOS Linux 7/x86_64
 
 yum -y install centos-release-scl
-yum -y install wget curl rsync sclo-vagrant1 libvirt qemu-kvm
+yum -y install wget curl rsync sclo-vagrant1 sclo-vagrant1-vagrant-libvirt libvirt qemu-kvm
 service libvirtd status
 if [ $? -ne 0 ]; then
   service libvirtd start
