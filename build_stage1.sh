30ece13444943c19cb8a8bcd8d8a941fdf0025a7
Colin Walters <walters@verbum.org>
2016-09-09 16:21:14 -0400

    stage1: Only generate a static delta if we have a previous commit
    Since people building this for the first time won't have one.

--- build_stage1.sh
+++ build_stage1.sh
@@ -65,7 +65,9 @@ ostree remote add --repo=/srv/repo centos-atomic-host --set=gpg-verify=false htt
 ## compose a new tree, based on defs in centos-atomic-host.json
 
 rpm-ostree compose --repo=${OstreeRepoDir} tree --add-metadata-string=version=${VERSION} ${GitDir}/centos-atomic-host.json |& tee ${BuildDir}/log.compose
-ostree --repo=${OstreeRepoDir} static-delta generate centos-atomic-host/7/x86_64/standard
+if ostree --repo=${OstreeRepoDir} rev-parse centos-atomic-host/7/x86_64/standard^ &>/dev/null; then
+    ostree --repo=${OstreeRepoDir} static-delta generate centos-atomic-host/7/x86_64/standard
+fi
 ostree --repo=${OstreeRepoDir} summary -u |& tee ${BuildDir}/log.compose
 
 # deal with https://bugzilla.gnome.org/show_bug.cgi?id=748959
