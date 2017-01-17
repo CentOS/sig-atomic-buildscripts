c32c25e745195cab8e18be2a17ba492b37c88bac
Jonathan Lebon <jlebon@redhat.com>
2017-01-17 17:43:11 -0500

    centos-ci: add smoketesting for the continuous branch
    Add a new job, atomic-tree-smoketest-centos7, which runs after every new
    tree compose. This job runs the improved sanity test from the
    atomic-host-tests suite:
    
    https://github.com/projectatomic/atomic-host-tests/tree/master/tests/improved-sanity-test
    
    If the test passes, we bump the smoketested rev, which is stored on the
    slave's home directory, for lack of a better place. Whenever the
    treecompose runs, we pick up the rev and insert it in releases.yaml so
    that do-release-tags does the needful.
    
    Disclaimer: About 13.8% of this code is untested.

---
+++ centos-ci/libvm.sh
@@ -0,0 +1,51 @@
+# Based on projectatomic/rpm-ostree libvm.sh
+
+vm_setup() {
+    ip=$1; shift
+    SSH="ssh -o UserKnownHostsFile=/dev/null \
+             -o StrictHostKeyChecking=no \
+             root@$ip"
+}
+
+# run command in vm
+# - $@    command to run
+vm_cmd() {
+  $SSH "$@"
+}
+
+# wait until ssh is available on the vm
+# - $1    timeout in second (optional)
+# - $2    previous bootid (optional)
+vm_ssh_wait() {
+  timeout=${1:-0}; shift
+  old_bootid=${1:-}; shift
+  while [ $timeout -gt 0 ]; do
+    if bootid=$(vm_get_boot_id 2>/dev/null); then
+        if [[ $bootid != $old_bootid ]]; then
+            return 0
+        fi
+    fi
+    if test $(($timeout % 5)) == 0; then
+        echo "Still failed to log into VM, retrying for $timeout seconds"
+    fi
+    timeout=$((timeout - 1))
+    sleep 1
+  done
+  if ! vm_cmd true; then
+     echo "Failed to log into VM, retrying with debug:"
+     $SSH -o LogLevel=debug true || true
+  fi
+  false "Timed out while waiting for SSH."
+}
+
+vm_get_boot_id() {
+  vm_cmd cat /proc/sys/kernel/random/boot_id
+}
+
+# reboot the vm
+vm_reboot() {
+  bootid=$(vm_get_boot_id 2>/dev/null)
+  vm_cmd systemctl reboot || :
+  vm_ssh_wait 120 $bootid
+}
+
