022766a280b37046e451dc55700e9e96f325d7ce
Jonathan Lebon <jlebon@redhat.com>
2017-01-23 10:57:57 -0500

    jjb: transfer repos via rsync --delete
    The run-treecompose task edits its `releases.yml` before rsync'ing it to
    the build node. However, the `git reset` there would wipe it out. Rather
    than doing `git reset`, switch over to `rsync --delete` when
    transferring repos so that we're sure the target repo is clean in the
    event the node is being reused from a previous task.

--- centos-ci/libtask.sh
+++ centos-ci/libtask.sh
@@ -18,9 +18,6 @@ prepare_job() {
 
     . ~/rsync-password.sh
 
-    # Ensure we're operating on a clean base
-    (cd ${buildscriptsdir} && git clean -dfx && git reset --hard HEAD)
-
     # Work around https://lists.centos.org/pipermail/ci-users/2016-July/000302.html
     for file in config.ini atomic-centos-continuous.repo cahc.tdl cloud.ks vagrant.ks pxelive.ks; do
         sed -i -e 's,https://ci.centos.org/artifacts/,http://artifacts.ci.centos.org/,g' ${buildscriptsdir}/${file}
